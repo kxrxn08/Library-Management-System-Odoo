27acad41035a17864fd70468e7615b3f8c2b99a1
Vinit-180 <121872021+Vinit-180@users.noreply.github.com>
2024-07-14 11:52:53 +0530

    feat: frontend admin book insertion

--- frontend/src/Pages/Admin.jsx
+++ frontend/src/Pages/Admin.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Modal from "../Components/Modal";
+import axios from 'axios';
 const Admin = () => {
+    const [books,setBooks]=useState([]);
+
+    useEffect(()=>{
+
+        axios.get("http://localhost:9999/api/books/getbooks").then((data)=>{
+            console.log(data.data.data);
+            setBooks(data.data.data);
+        }).catch((error)=>{
+            console.log(error);
+        })
+
+    },[]);
   return (
     <div className='container m-auto mt-5 '>
         <div className='container ms-3 grid grid-cols-2 ' >
@@ -9,9 +22,13 @@ const Admin = () => {
             </form>
             <div className='col'>
                 <Modal/>
-                {/* <button className='bg-green-600 px-3 py-2 rounded-lg' type='button'>Add New Book</button> */}
             </div>
         </div>
+        {/* <div className="container">
+            {
+                books.map((e,index)=>{});
+            }
+        </div> */}
 
     </div>
   )
